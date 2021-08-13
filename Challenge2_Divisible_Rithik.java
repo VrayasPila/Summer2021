cbdbf7856fca38ea97982c5417f6aaa5ea745adb
myusername <sweetynranai@gmail.com>
2021-08-12 20:47:13 -0500

    Day 4 and peoples challenges

--- Challenge2_Divisible_Rithik.java
+++ Challenge2_Divisible_Rithik.java
@@ -1,4 +1,4 @@
-package Summer2021;
+ 
 import java.util.*;
 
 /**
@@ -18,17 +18,18 @@ import java.util.*;
 public class Challenge2_Divisible_Rithik
 {
     public static void main(String args[])
-    {Scanner sc = new Scanner(System.in);
-    System.out.println("Select a number");
-    int num1 = sc.nextInt();
-    System.out.println("Select another number");
-    int num2 = sc.nextInt();
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select a number");
+        int num1 = sc.nextInt();
+        System.out.println("Select another number");
+        int num2 = sc.nextInt();
     //variable assignment
     
     if (num1%num2 == 0)
-    System.out.println("These two numbers are divisible");
+        System.out.println("These two numbers are divisible");      
     else 
-    System.out.println("These two numbers are not divisible");
+        System.out.println("These two numbers are not divisible");
     // Conditions for input
     }
 }
---
+++ Challenges/Challenge_Day4/Challenge_Day4.java
@@ -0,0 +1,18 @@
+package Challenges.Challenge_Day4;
+
+
+/**
+ * 
+ * C4.1 You are making a system that describes what kind of number a number is. You have a few options. 
+ *      
+ * 
+ * Rules
+       1. Have variable names that make sense
+       2. Follow one convention systematically. If your brackets start on the next line as mine do, Do not change that
+       3. Try to make the code as concise as possible. Not the least line numbers but logically, as simple as it can be
+       4. Make the program funny or sarcastic bc why not. 
+ */
+public class Challenge_Day4
+{
+    
+}

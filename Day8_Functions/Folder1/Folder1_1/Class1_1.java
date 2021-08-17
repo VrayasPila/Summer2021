a2c4c2bda8538282c015ca91c50f1b13a9f14c4c
myusername <sweetynranai@gmail.com>
2021-08-16 19:51:44 -0500

    Day 8

---
+++ Day8_Functions/Folder1/Folder1_1/Class1_1.java
@@ -0,0 +1,34 @@
+package Day8_Functions.Folder1.Folder1_1;
+
+
+/**
+ * Write a description of class Class1_1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Class1_1
+{
+    // instance variables - replace the example below with your own
+    private static int class1_1private = 10;
+    protected static int class1_1protected = 11;
+             static int class1_1default = 12;
+    public static int class1_1public = 13;
+
+    /**
+     * Constructor for objects of class Class1_1
+     */
+    public Class1_1()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+   
+}
---
+++ Day8_Functions/Scope.java
@@ -0,0 +1,12 @@
+package Day8_Functions;
+
+import Folder1.*;
+
+public class Scope
+{
+   
+    public static void main(String args[])
+    {
+        System.out.println(Folder1.Class1_1.class1_1proteteced);
+    }
+}
